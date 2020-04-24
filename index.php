726838a3ac69eacb3df1155686d8d75f13ebca09
Medr3x <leonardo@meemba.com.ar>
2020-04-24 11:54:02 -0300

    a

--- index.php
+++ index.php
@@ -257,7 +257,7 @@
                                                 $15.000
                                             </div>
                                         </div>
-                                        <form action="/detail.php" method="get">
+                                        <form action="https://fast-mp.herokuapp.com/detail/index.php" method="get">
                                             <input type="hidden" name="img" value="assets/samsung-galaxy-s9-xxl.jpg">
                                             <input type="hidden" name="title" value="Samsung Galaxy S9">
                                             <input type="hidden" name="price" value="15000">
@@ -314,7 +314,7 @@
                                                 $10.000
                                             </div>
                                         </div>
-                                        <form action="/detail.php" method="get">
+                                        <form action="https://fast-mp.herokuapp.com/detail/index.php" method="get">
                                             <input type="hidden" name="img" value="assets/l6g6.jpg">
                                             <input type="hidden" name="title" value="LG G6">
                                             <input type="hidden" name="price" value="10000">
@@ -374,7 +374,7 @@
                                                 </span>
                                             </div>
                                         </div>
-                                        <form action="/detail.php" method="get">
+                                        <form action="https://fast-mp.herokuapp.com/detail/index.php" method="get">
                                             <input type="hidden" name="img" value="assets/u_10168742.jpg">
                                             <input type="hidden" name="title" value="iPhone 8">
                                             <input type="hidden" name="price" value="16000">
@@ -433,7 +433,7 @@
                                                 </span>
                                             </div>
                                         </div>
-                                        <form action="/detail.php" method="get">
+                                        <form action="https://fast-mp.herokuapp.com/detail/index.php" method="get">
                                             <input type="hidden" name="img" value="assets/motorola-moto-g5-plus-1.jpg">
                                             <input type="hidden" name="title" value="Motorola G5">
                                             <input type="hidden" name="price" value="9000">
@@ -485,7 +485,7 @@
                                                 $8.000
                                             </div>
                                         </div>
-                                        <form action="/detail.php" method="get">
+                                        <form action="https://fast-mp.herokuapp.com/detail/index.php" method="get">
                                             <input type="hidden" name="img" value="assets/motorola-moto-g4-3.jpg">
                                             <input type="hidden" name="title" value="Moto G4">
                                             <input type="hidden" name="price" value="8000">
@@ -538,7 +538,7 @@
                                                 </span>
                                             </div>
                                         </div>
-                                        <form action="/detail.php" method="get">
+                                        <form action="https://fast-mp.herokuapp.com/detail/index.php" method="get">
                                             <input type="hidden" name="img" value="assets/003.jpg">
                                             <input type="hidden" name="title" value="Sony Xperia XZ2">
                                             <input type="hidden" name="price" value="10000">
